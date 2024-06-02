e33b52906af50f8899076f1f429ca3c29872b606
Nikhil Lakhwani <nikhilnlakhwani@gmail.com>
2024-06-02 18:41:45 -0400

    Added Art struct and state variables to state.rs

--- cosmwasm/src/state.rs
+++ cosmwasm/src/state.rs
@@ -2,12 +2,21 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+
+pub struct Art {
+    pub art_id: u64,
+    pub price: u64,
+    pub rfid: u64,
+}
+
 pub struct State {
     pub count: i32,
     pub owner: Addr,
 }
 
 pub const STATE: Item<State> = Item::new("state");
+pub const GALLERY: Map<u64, Art> = Map::new("gallery");
+pub const OWNERS: Map<u64, Addr> = Map::new("owners");
